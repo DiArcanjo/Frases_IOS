6733142b6bab35a816b0ff7a2eb570c142185613
Diana Arcanjo <dianaarcanjo.dev@gmail.com>
2019-05-27 15:37:51 -0300

    App Frases

--- Frases do Dia/ViewController.swift
+++ Frases do Dia/ViewController.swift
@@ -9,7 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    
+    @IBAction func novaFrase(_ sender: Any) {
+        
+        var frases: [String] = []
+        
+        frases.append("Belive in yourself")
+        frases.append("Be Strong")
+        frases.append("Never Giveup")
+        
+        let gerandoFrasesAleatorias = arc4random_uniform(3)
+        
+        legendaResultado.text = frases[Int(gerandoFrasesAleatorias)]
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
